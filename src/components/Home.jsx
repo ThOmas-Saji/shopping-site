84015978bcb508d7efabcb0d95de932feadca6fc
Thomas-Saji <thomas.saji@accubits.com>
2023-12-28 12:53:27 +0530

    added env

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -40,9 +40,7 @@ const Home = () => {
   };
   const handleGetData = async () => {
     try {
-      const { data } = await axios.get(
-        "https://run.mocky.io/v3/f47694b8-4d45-4c30-aed0-dd82bb4025fb"
-      );
+      const { data } = await axios.get(process.env.REACT_APP_API);
       setMenuData(data?.data[0]);
     } catch (error) {
       console.log(error);
